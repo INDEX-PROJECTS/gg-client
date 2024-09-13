a3ecc06d98f7af608a8d75133c723ebe158447b1
nayvok <44442827+nayvok@users.noreply.github.com>
2024-09-13 20:32:19 +0500

    feat: add products data

--- src/shared/lib/types/types.ts
+++ src/shared/lib/types/types.ts
@@ -3,3 +3,12 @@ import { ReactNode } from 'react';
 export type Children = {
   children?: ReactNode;
 };
+
+export interface IProduct {
+  id: number;
+  title: string;
+  description: string | null;
+  price: string;
+  vendorCode: string;
+  image: string | null;
+}
