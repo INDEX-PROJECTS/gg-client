237a95e28fbdf84a493b51c60b98a36de06f7ef5
nayvok <44442827+nayvok@users.noreply.github.com>
2024-09-14 15:07:29 +0500

    feat: add categories namespaces

---
+++ src/shared/lib/constants/categories.ts
@@ -0,0 +1,34 @@
+export const categories: { name: string; link: string }[] = [
+  {
+    name: 'Электродвигатели',
+    link: 'electric-engines',
+  },
+  {
+    name: 'Насосы',
+    link: 'pumps',
+  },
+  {
+    name: 'Частотные преобразователи и устройства плавного пуска',
+    link: 'invertors',
+  },
+  {
+    name: 'Редукторы и мотор-редукторы',
+    link: 'gearbox',
+  },
+  {
+    name: 'Подшипники',
+    link: 'bearings',
+  },
+  {
+    name: 'Климатическое оборудование',
+    link: 'klimaticheskoe-oborudovanie',
+  },
+  {
+    name: 'Электрощитовое оборудование',
+    link: 'schild',
+  },
+  {
+    name: 'Тали',
+    link: 'tali',
+  },
+];
