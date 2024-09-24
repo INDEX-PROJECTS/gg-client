bce4863826a0142fa01e17b6120f613ccca049e4
nayvok <44442827+nayvok@users.noreply.github.com>
2024-09-24 17:56:09 +0500

    fix: add suspens for useSearchParams

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -1,7 +1,12 @@
 import DocumentsPage from '@/src/widgets/documentsPage/documentsPage';
+import { Suspense } from 'react';
 
 const Page = () => {
-  return <DocumentsPage />;
+  return (
+    <Suspense fallback={null}>
+      <DocumentsPage />
+    </Suspense>
+  );
 };
 
 export default Page;
