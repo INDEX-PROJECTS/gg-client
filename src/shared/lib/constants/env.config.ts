e8fbd7311c6eb6fc25c7347352c56b212455258f
nayvok <44442827+nayvok@users.noreply.github.com>
2024-12-23 23:15:54 +0500

    feat: add domain name in env

--- src/shared/lib/constants/env.config.ts
+++ src/shared/lib/constants/env.config.ts
@@ -1,2 +1,3 @@
 export const ENABLED_PAYMENT = process.env.NEXT_PUBLIC_ENABLED_PAYMENT || '';
 export const ALTERNATIVE_CATEGORIES = process.env.NEXT_PUBLIC_ALTERNATIVE_CATEGORIES || '';
+export const DOMAIN_NAME = process.env.NEXT_PUBLIC_DOMAIN_NAME || '';
